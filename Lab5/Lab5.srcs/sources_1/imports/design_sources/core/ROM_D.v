b207066cf37b33d9f91c7a02c222e32fd7879976
Axin Brave <3220104364@zju.edu.cn>
2024-11-25 14:41:12 +0800

    Finish Lab5

---
+++ Lab5/Lab5.srcs/sources_1/imports/design_sources/core/ROM_D.v
@@ -0,0 +1,16 @@
+`timescale 1ns / 1ps
+
+module ROM_D(
+    input[6:0] a,
+    output[31:0] spo
+);
+
+    reg[31:0] inst_data[0:127];
+
+    initial	begin
+        $readmemh("D:/CS/Verilog-studying/CA/Lab5_doc/lab5/riscv_sources/rom.hex", inst_data);
+    end
+
+    assign spo = inst_data[a];
+
+endmodule
---
+++ Lab5_doc/Lab5/riscv_sources/readme.txt
@@ -0,0 +1,2 @@
+请将riscv.txt中的riscv代码转为机器码，并生成对应的rom.hex文件
+ram.hex和rom.hex请按Lab1 PPT中的指南导入工程并使用
